e95b2fe55ae2c05800cb3d1f94726e6b53d66ef8
Nilesh Kumar <19nilesh.kumar@gmail.com>
2025-06-06 00:36:12 +0530

    Day3

--- Practice/Day3.js
+++ Practice/Day3.js
@@ -1,17 +1,35 @@
-let n = -123908;
+let num = 2552;
 
 function getNumberCount(n) {
   // Handling zero
   if (n == 0) return 1;
   // Handling negative numbers
   n = Math.abs(n);
+  let newN = n;
   let count = 0;
   while (n > 0) {
     n = Math.floor(n / 10);
     count++;
   }
-  return count;
+  return `No. of digits in ${newN} is ${count}`;
 }
-console.log(getNumberCount(n));
+console.log(getNumberCount(num));
 
 //Palindrome number check
+function isPalindrome(n) {
+  if (n == 0) return true;
+  if (n < 0) return false;
+  let newN = n;
+  let rev = 0;
+  while (newN > 0) {
+    let rem = newN % 10;
+    rev = 10 * rev + rem;
+    newN = Math.floor(newN / 10);
+  } // returns reverse of a number
+  if (rev == n) {
+    return `${n} is palindrome`;
+  } else {
+    return `${n} is not palindrome`;
+  }
+}
+console.log(isPalindrome(num));
