ade8a3c7baf9d01c28e19b56ebc04b962ec215d0
Nilesh Kumar <19nilesh.kumar@gmail.com>
2025-06-21 14:00:49 +0530

    Fix:Binary search

--- Practice/Day7-Searching.js
+++ Practice/Day7-Searching.js
@@ -1,24 +1,25 @@
-//Linear search
-const arr = [0, 1, 2, 9, 7];
-function linearSearch(arr, n) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == n) {
-      return i;
-    }
-  }
-  return -1;
-}
+// //Linear search
+const arr = [0, 1, 2, 7, 9];
+// function linearSearch(arr, n) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == n) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
 
 // Binary Search
 
 function binarySearch(arr, target) {
+  // Works on sorted array and complexity is O(log n)
   let left = 0;
   let right = arr.length - 1;
   while (right >= left) {
     let middle = Math.floor((left + right) / 2);
     if (target == arr[middle]) {
       return middle;
-    } else if (target < middle) {
+    } else if (target < arr[middle]) {
       right = middle - 1;
     } else {
       left = middle + 1;
@@ -27,4 +28,4 @@ function binarySearch(arr, target) {
   return -1;
 }
 
-console.log(binarySearch(arr, 1));
+console.log(binarySearch(arr, 9));
