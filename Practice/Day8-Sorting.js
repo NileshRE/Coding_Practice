3c0d18e53d4f67f7f11ae36e6f5d94b3aea2e035
Nilesh Kumar <19nilesh.kumar@gmail.com>
2025-06-22 17:49:13 +0530

    ADD:Comment in functions

--- Practice/Day8-Sorting.js
+++ Practice/Day8-Sorting.js
@@ -6,6 +6,7 @@ function bubbleSort(arr) {
     let isSwapped = false; // If in any iteration no swap happens, stop sorting which can reduce time complexity.
     for (j = 0; j < n - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
+        // Keep swaping if arr[j] is greater than arr[j+1]
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
@@ -22,13 +23,16 @@ function selectionSort(arr) {
   // Time Complexity - O(n*n) Space Complexity - O(1)
   let n = arr.length;
   for (let i = 0; i < n - 1; i++) {
-    let min = i;
+    let min = i; // Take min as i=1;
     for (j = i + 1; j < n; j++) {
+      //Loop through i+1 till last element
       if (arr[j] < arr[min]) {
+        // If
         min = j;
       }
     }
     if (min != i) {
+      // Swap only if current and next element are not same
       let temp = arr[min];
       arr[min] = arr[i];
       arr[i] = temp;
@@ -42,13 +46,15 @@ function selectionSort(arr) {
 function insertionSort(arr) {
   let n = arr.length; // Time Complexity - O(n*n) Space Compelxity - O(1)
   for (let i = 1; i < n; i++) {
-    let curr = arr[i];
+    // Start from i=1
+    let curr = arr[i]; // Take arr[1] as current and prev as i-1
     let prev = i - 1;
     while (arr[prev] > curr && prev >= 0) {
-      arr[prev + 1] = arr[prev];
-      prev--;
+      // Till previous element is greater than current element and
+      arr[prev + 1] = arr[prev]; // previous is greater than equal to 0, swap values assign greater value to prev+1
+      prev--; // move pointer to prev element position minus 1
     }
-    arr[prev + 1] = curr;
+    arr[prev + 1] = curr; // After end of while loop if last element is 2nd smaller and pointer is on smallest, place it there.
   }
   return arr;
 }
@@ -56,6 +62,7 @@ function insertionSort(arr) {
 // Merge Sort (Divide and Conquer)
 
 function mergeSort(arr) {
+  // TIme Complexity - O(nlogn) - Recursion and sorting Space Complexity - O(n)
   if (arr.length <= 1) return arr; // Base condition for stopping recursion
   let mid = Math.floor(arr.length / 2); // Find the mid point of array
   let left = mergeSort(arr.slice(0, mid)); // Dividing in 2 half - left and right
