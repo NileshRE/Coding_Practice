f79afa17a275232904d475b2eb05cd03d9172c44
Nilesh Kumar <19nilesh.kumar@gmail.com>
2025-11-06 23:41:13 +0530

    ADD: Implement removeOutermostParentheses function to process nested parentheses

---
+++ Practice/Day22-Stacks.js
@@ -0,0 +1,21 @@
+function removeOutermostParentheses(s) {
+  let stack = [];
+  let ans = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      // If opening bracket push in stack
+      stack.push(i);
+      if (stack.length > 1) {
+        // If stack length is greater than 1, push into answer since we have to remove outermost bracket
+        ans += s[i];
+      }
+    } else {
+      if (stack.length > 1) {
+        // Do the same when we encounter closing bracket
+        ans += s[i];
+      }
+      stack.pop(); // pop from stack when closing bracket is encountered
+    }
+  }
+  return ans;
+}
